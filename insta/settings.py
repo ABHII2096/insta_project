29ddc1ad7d1f120580c8a331e810ed48f2c1f02b
Mohd-Ashif0770 <mohdashif0770@gmail.com>
2025-12-02 12:49:20 +0530

    ready to pushh

--- insta/settings.py
+++ insta/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p_&$w^rurw)j7nf593q6ny1qssg6-0n@vqki#suabp(@^0vrp^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.5']
+ALLOWED_HOSTS = ['*']
 
 
 
